5f3d8c76509d6b7eb22d2da000cb769faad5d8d7
Daniel Choi <schoiak@connect.ust.hk>
2022-02-28 01:41:31 +0900

    Refactor - Decomposed DB handling, error handling

--- api/saeroun-api.py
+++ api/saeroun-api.py
@@ -1,34 +1,20 @@
-from flask import Flask
-from flask_restful import Api, Resource, abort
-from pymongo import MongoClient
-import json
-from bson import json_util
-from arg_parser import REGISTER_ARGS_PARSER, LOGIN_ARGS_PARSER
+from ast import Raise
+from flask import Flask, jsonify
+from flask_restful import Api, Resource
 
-from config import API_RETURN_CODE_DESC, MONGO_DATA
-from literal import LIT
 
-client = MongoClient(
-    host=MONGO_DATA[LIT.HOST], 
-    port=MONGO_DATA[LIT.PORT], 
-    username=MONGO_DATA[LIT.USERNAME], 
-    password=MONGO_DATA[LIT.PASSWORD]
-)
-db = client[LIT.SAEROUN]
+from lib.arg_parser import REGISTER_ARGS_PARSER, LOGIN_ARGS_PARSER
+from lib.common import to_json
+
+from lib.literal import LIT, API_RETURN_CODE_DESC
+
+import sys
+
+from lib.query_handler import find_from_email, mongo_query
 
 app = Flask(__name__)
 api = Api(app)
 
-def to_json(data):
-    return json.loads(json_util.dumps(data))
-
-def mongo_find_one(collection, query):
-    result = db[collection].find_one(query)
-    return to_json(result)
-
-def mongo_insert_one(collection, data):
-    result = db[collection].insert_one(data)
-    return result.acknowledged
 
 def get_description(return_code):
     description = API_RETURN_CODE_DESC.get(return_code)
@@ -41,10 +27,10 @@ class register(Resource):
         # Parse post data
         args = REGISTER_ARGS_PARSER.parse_args()
         # Check if email can be used
-        result = mongo_find_one(collection=LIT.USER, query={LIT.EMAIL: args[LIT.EMAIL]})
-        
+        query_result = find_from_email(args[LIT.EMAIL])
+
         # if email is already in use
-        if result != None:
+        if query_result != None:
             api_result = LIT.FAILED
             return_code = 101 
         # if password confirmation failed
@@ -55,29 +41,26 @@ class register(Resource):
             # remove password confirmation data
             del(args[LIT.PASSWORD_CONFIRM])
             # insert user data
-            acknowledged = mongo_insert_one(collection=LIT.USER, data=args)
-            # Invalid Mongo Operation
-            if not acknowledged:
-                api_result = LIT.FAILED,
-                return_code = 103
+            query_result = mongo_query(collection=LIT.USER, type=LIT.INSERT_ONE, data=args)
             # Successfully inserted
-            else:
-                api_result = LIT.SUCCESS,
-                return_code = 201
+            api_result = LIT.SUCCESS,
+            return_code = 201
         
-        return {
+        api_return = {
                 LIT.API_RESULT : api_result,
                 LIT.API_RETURN_CODE: return_code,
                 LIT.DESCRIPTION: get_description(return_code),
                 LIT.DATA: to_json(args)
         }
-
+        
+        return api_return
+    
 class login(Resource):
     def post(self):
         # Parse post data
         args = LOGIN_ARGS_PARSER.parse_args()
         # Check if email can be used
-        result = mongo_find_one(collection=LIT.USER, query={LIT.EMAIL: args[LIT.EMAIL]})
+        query_result = find_from_email(args[LIT.EMAIL])
         
         # Couldn’t find email
         if result is None:
@@ -102,9 +85,12 @@ class login(Resource):
                 LIT.DATA: to_json(result)
         }
             
+# api.add_resource(Video, "/video/<int:id>")
+
+
 api.add_resource(register, "/register")
 api.add_resource(login, "/login")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=5001)
+    app.run(host='0.0.0.0', debug=True, port=5000)
     
