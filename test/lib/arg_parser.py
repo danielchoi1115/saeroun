af6239173d195b347a85765e72a354b117509993
Daniel Choi <schoiak@connect.ust.hk>
2022-03-18 00:31:24 +0900

    Refactor - increased code reusability

--- test/lib/arg_parser.py
+++ test/lib/arg_parser.py
@@ -1,14 +1,44 @@
 from flask_restful import reqparse
 from lib.literal import LIT
+import json
 
 
-class argparser:
-    register = reqparse.RequestParser()
-    register.add_argument(LIT.EMAIL, type=str, help="", required=True)
-    register.add_argument(LIT.STUDENT_NAME, type=str, help="", required=True)
-    register.add_argument(LIT.PASSWORD, type=str, help="", required=True)
-    register.add_argument(LIT.PASSWORD_CONFIRM, type=str, help="", required=True)
+class argparser():
+    @classmethod
+    def user(self):
+        user = reqparse.RequestParser()
+        user.add_argument(LIT.EMAIL, type=str, help="", required=True)
+        user.add_argument(LIT.STUDENT_NAME, type=str, help="")
+        user.add_argument(LIT.PASSWORD, type=str, help="", required=True)
+        user.add_argument(LIT.PASSWORD_CONFIRM, type=str, help="")
+        user.add_argument(LIT.AUTHORIZATION, location='headers')
+        print(user.parse_args())
+        return user.parse_args()
 
-    login = reqparse.RequestParser()
-    login.add_argument(LIT.EMAIL, type=str, help="", required=True)
-    login.add_argument(LIT.PASSWORD, type=str, help="", required=True)
+
+# class argparser():
+#     @classmethod
+#     def user(self):
+#         user = reqparse.RequestParser()
+#         user.add_argument(LIT.EMAIL, type=str, help="", required=True)
+#         user.add_argument(LIT.STUDENT_NAME, type=str, help="")
+#         user.add_argument(LIT.PASSWORD, type=str, help="", required=True)
+#         user.add_argument(LIT.PASSWORD_CONFIRM, type=str, help="")
+#         user.add_argument(LIT.AUTHORIZATION, location='headers')
+#         print(user.parse_args())
+#         return args(**user.parse_args())
+
+
+# class args():
+#     # initializes a class similar to a tuple class.
+#     # every arguments passed from argparser() will be initialized as class attributes
+#     def __init__(self, **kwargs):
+#         self.__dict__.update(kwargs)
+
+#     def __repr__(self):
+#         return json.dumps(self.__dict__)
+
+#     # custom __getattr__ function
+#     # return None if attribute does not exist instead of AttributeError
+#     def __getattr__(self, __name):
+#         return object.__getattribute__(self, __name) if __name in self.__dict__ else None
