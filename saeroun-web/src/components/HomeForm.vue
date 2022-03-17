cf6f57c0ef58e1cdb4d1427b4b4e6148f62e668f
Daniel Choi <schoiak@connect.ust.hk>
2022-03-18 00:26:22 +0900

    Rename - fixed typo in the filename HomeFomr

---
+++ saeroun-web/src/components/HomeForm.vue
@@ -0,0 +1,41 @@
+<template>
+  <!-- <form @submit.prevent="signin"> -->
+  <form>
+    <label>Email</label>
+    <input type="email" required />
+
+    <label>Name</label>
+    <input type="name" required />
+
+    <div class="submit">
+      <button>Get User Info</button>
+    </div>
+  </form>
+  <p>Email: {{ email }}</p>
+  <p>Name: {{ name }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      name: '',
+    }
+  },
+  methods: {
+    // signin() {
+    //   authApi
+    //     .signin(this.email, this.password)
+    //     .then(response => {
+    //       console.log(response.data)
+    //     })
+    //     .catch(error => {
+    //       console.log(error.response.data)
+    //     })
+    // },
+  },
+}
+</script>
+
+<style></style>
