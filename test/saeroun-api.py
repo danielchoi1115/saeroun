8089705cfec4e0d8f67c5d8a606aa5db266fdff0
Daniel Choi <schoiak@connect.ust.hk>
2022-03-18 00:34:39 +0900

    Feat - User authentication added with JWT

--- test/saeroun-api.py
+++ test/saeroun-api.py
@@ -1,7 +1,10 @@
 # dependencies
-from flask import Flask, make_response, request, redirect
+from flask import Flask, jsonify, make_response, request, redirect
 from flask_restful import Api, Resource
 from flask_cors import CORS
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token, get_jwt_identity, unset_jwt_cookies, create_refresh_token, set_access_cookies, set_refresh_cookies
+)
 
 # built-in
 # import ssl
@@ -9,62 +12,125 @@ from flask_cors import CORS
 # Custom
 from lib.arg_parser import argparser
 from lib.common import get_description
-from lib.config import confidential
+from lib.config import appconfig
 from lib.literal import LIT
 from lib.query_handler import find_user_by_email, mongo_query
-from lib.auth import YellowToken
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = confidential.API_SECRET_KEY
-app.config['CORS_HEADERS'] = 'Content-Type'
+app.config.from_object(appconfig)
 
+jwt = JWTManager(app)
 api = Api(app)
-# , resources={r"*": {"origins": "*"}}
-
-CORS(app, resources={r"*": {"origins": '175.194.158.210'}})
+CORS(app, resources={r"*": {"origins": '172.30.1.100:8080'}}, supports_credentials=True)
 
 
 class user(Resource):
-    def post(self, keyword):
-        if keyword == LIT.AUTH:
-            args = argparser.login.parse_args()  # Parse post data
-            result, status_code = user_post_auth(args)
-            # res =
-            # if status_code == 200:
-            #     res.set_cookie(
-            #         key=LIT.ACCESS_TOKEN,
-            #         value=result[LIT.ACCESS_TOKEN],
-            #         httponly=True,
-            #         domain='175.194.158.210',
-            #     )
-            if LIT.ACCESS_TOKEN in result:
-                return make_response(result, status_code)
-        elif keyword == LIT.NEW:
-            args = argparser.register.parse_args()  # Parse post data
-            return user_post_new(args)
+    # def get(self):
+    #     # with token:
+    #     #   return user information (only for the teacher)
+
+    #     # without token:
+    #     #   return invalid operation
+
+    #     # args = argparser.post_user.parse_args()  # Parse post data
+    #     # result, status_code = post_user(args)
+    #     # if LIT.ACCESS_TOKEN in result:
+    #     #     return make_response(result, status_code)
+    #     access_token = create_access_token(identity='haha'),
+    #     refresh_token = create_refresh_token(identity='hoho')
+    #     resp = {'login': True}
+    #     set_access_cookies(resp, access_token)
+    #     set_refresh_cookies(resp, refresh_token)
+    #     return {
+    #         "access": access_token,
+    #         "refresh": refresh_token
+    #     }
+
+    def post(self):
+
+        args = argparser().user()  # Parse post data
+
+        email = args['email']
+        student_name = args['student_name']
+        password = args['password']
+
+        # check if arg.email is there:
+        query_result = find_user_by_email(email)
+        data = {}
+
+        # if user does not exist
+        if not query_result:
+            # and if student name is given, create user
+            if student_name:
+                data['info'] = 'Create a new user'
+            # if student name is not given (if only email and password is given)
+            # redirect to sign up page
+            elif password:
+                data['info'] = 'Email or password wrong'
+        # if user already exist
+        else:
+            if student_name:
+                data['info'] = 'User already exist'
+            else:
+                # if user authentication is valid
+                if query_result['password'] == password:
+                    data['info'] = 'Return Tokens'
+                    data['access_token'] = create_access_token(identity={'email': email})
+                    data['refresh_token'] = create_refresh_token(identity=email)
+                else:
+                    data['info'] = 'Email or password wrong'
+
+        #   return token (sign in)
+        # if no user exist:
+        #   redirects to the sign up page.
+        # with extra information:
+        #   creates a new user (sign up)
+
+        response = make_response(data, 200)
+        if 'access_token' in data:
+            set_access_cookies(response, data['access_token'])
+        if 'refresh_token' in data:
+            set_refresh_cookies(response, data['refresh_token'])
+
+        return response
+
+        # return post_user(args)
 
 
-def user_post_auth(args):
+def post_user(args):
+
     # Get the info by email
     query_result = find_user_by_email(args[LIT.EMAIL])
+    matched = args[LIT.PASSWORD] == query_result[LIT.PASSWORD]
 
-    # Could’t find email or password does not match
-    if (not query_result) or (args[LIT.PASSWORD] != query_result[LIT.PASSWORD]):
+    # if user exist:
+    #     return token (sign in)
+
+    # case 1 - user exist
+    if query_result and matched:
+        return {
+            LIT.API_RESULT: LIT.SUCCESS,
+            LIT.DESCRIPTION: get_description(202),  # Login Successful
+            # LIT.ACCESS_TOKEN: YellowToken().create_token(data=query_result['_id'])
+        }, 200  # HTTP OK
+    elif not matched:
         return {
             LIT.API_RESULT: LIT.FAILED,
             LIT.DESCRIPTION: get_description(104),  # Incorrect email or password
         }, 401  # Unauthorized
+    # elif user not exist:
+    #     if 4 args are given
+    #         creates a new user (sign up)
 
-    yellowtoken = YellowToken()
-    return {
-        LIT.API_RESULT: LIT.SUCCESS,
-        LIT.DESCRIPTION: get_description(202),  # Login Successful
-        LIT.ACCESS_TOKEN: yellowtoken.create_token(data=query_result['_id'])
-    }, 200  # HTTP OK
+    #     else (only email and password sent:)
+    #         redirects to the sign up page.
+
+    # Could’t find email or password does not match
+    # if (not query_result) or (args[LIT.PASSWORD] != query_result[LIT.PASSWORD]):
 
 
-def user_post_new(args):
+def get_user(args):
     # Check if email can be used
     query_result = find_user_by_email(args[LIT.EMAIL])
 
@@ -86,7 +152,7 @@ def user_post_new(args):
         }, 401  # Unauthrorized
 
 
-api.add_resource(user, "/api/user/<string:keyword>")
+api.add_resource(user, "/api/user")
 
 # @app.before_request
 # def before_request():
@@ -96,13 +162,9 @@ api.add_resource(user, "/api/user/<string:keyword>")
 #         return redirect(url, code=code)
 
 
-@app.route('/api/haha')
-def haha():
-    return 'hello'
-
-
 if __name__ == '__main__':
     # ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
     # ssl_context.load_cert_chain(certfile='/usr/share/saeroun/saeroun-web/ssl/domain.com.crt', keyfile='/usr/share/saeroun/saeroun-web/ssl/domain.com.key', password='tmdfuf3752!')
     # app.run(host='0.0.0.0', debug=True, port=5002, ssl_context=ssl_context)
     app.run(host='127.0.0.1', debug=True, port=5002)
+    # app.run(host='0.0.0.0', debug=True, port=5002)
