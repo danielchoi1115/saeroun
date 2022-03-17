8ad78628fa10bfedb360f332ffdd388ce6554d80
Daniel Choi <schoiak@connect.ust.hk>
2022-03-18 00:27:23 +0900

    Refactor - removed vue-cookie from main

--- saeroun-web/src/main.js
+++ saeroun-web/src/main.js
@@ -3,21 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import { VueCookieNext } from 'vue-cookie-next'
-
 const app = createApp(App)
 app.use(store)
 app.use(router)
-app.use(VueCookieNext)
 
 app.mount('#app')
-
-// set default config
-VueCookieNext.config({ expire: '7d' })
-
-// set global cookie
-VueCookieNext.setCookie('theme', 'default')
-VueCookieNext.setCookie('hover-time', { expire: '1s' })
 
 // var user = {
 //   id: 1,
