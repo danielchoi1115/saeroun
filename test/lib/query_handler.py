05a122dca5358971b02633a4f1f21c2b1aab14bc
Daniel Choi <schoiak@connect.ust.hk>
2022-03-23 20:30:49 +0900

    Test - Modularized

--- test/lib/query_handler.py
+++ test/lib/query_handler.py
@@ -5,28 +5,29 @@ from lib.error_handler import abort_if_query_failed
 
 from pymongo import MongoClient
 
-client = MongoClient(
-    host=MONGO_DATA[LIT.HOST],
-    port=MONGO_DATA[LIT.PORT],
-    username=MONGO_DATA[LIT.USERNAME],
-    password=MONGO_DATA[LIT.PASSWORD]
-)
-db = client[LIT.SAEROUN]
 
+class YellowMongo:
+    def __init__(self):
+        self.client = MongoClient(
+            host=MONGO_DATA[LIT.HOST],
+            port=MONGO_DATA[LIT.PORT],
+            username=MONGO_DATA[LIT.USERNAME],
+            password=MONGO_DATA[LIT.PASSWORD]
+        )
+        self.db = self.client[LIT.SAEROUN]
 
-def find_user_by_email(email):
-    return mongo_query(collection=LIT.USER, type=LIT.FIND_ONE, query={LIT.EMAIL: email})
+    def find_user_by_email(self, email):
+        return self.mongo_query(collection=LIT.USER, type=LIT.FIND_ONE, query={LIT.EMAIL: email})
 
+    def mongo_query(self, collection, type, **kwargs):
+        try:
+            if type == LIT.INSERT_ONE:
+                result = self.db[collection].insert_one(kwargs[LIT.DATA])
+                return result.acknowledged
 
-def mongo_query(collection, type, **kwargs):
-    try:
-        if type == LIT.INSERT_ONE:
-            result = db[collection].insert_one(kwargs[LIT.DATA])
-            return result.acknowledged
+            elif type == LIT.FIND_ONE:
+                result = self.db[collection].find_one(kwargs[LIT.QUERY])
+                return to_json(result)
 
-        elif type == LIT.FIND_ONE:
-            result = db[collection].find_one(kwargs[LIT.QUERY])
-            return to_json(result)
-
-    except Exception as ex:
-        abort_if_query_failed(ex)
+        except Exception as ex:
+            abort_if_query_failed(ex)
