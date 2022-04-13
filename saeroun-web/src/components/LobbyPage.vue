c7d865dcc766b7de71ee283b78ffc28f1b5ffd08
Daniel Choi <schoiak@connect.ust.hk>
2022-04-13 17:50:42 +0900

    Rename - renamed to LobbyPage

---
+++ saeroun-web/src/components/LobbyPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="submit">
+    <button>Get User Name</button>
+  </div>
+  <div>
+    <p>username: {{ userdata.username }}</p>
+    <p>email: {{ userdata.email }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    userdata: Object({
+      email: String,
+      username: String,
+      account_type: String,
+    }),
+  },
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style></style>
