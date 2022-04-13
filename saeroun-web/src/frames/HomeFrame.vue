ce424a41a22993e957ec556ed4b9f45196adce4f
Daniel Choi <schoiak@connect.ust.hk>
2022-04-13 17:59:14 +0900

    Test - added suspense feature

---
+++ saeroun-web/src/frames/HomeFrame.vue
@@ -0,0 +1,27 @@
+<template>
+  <div v-if="userData.result"><LobbyPage :userdata="userData.data" /></div>
+  <div v-else><LoginPage /></div>
+</template>
+
+<script>
+import { ref, defineAsyncComponent } from 'vue'
+import * as auth from '@/api/auth'
+
+const LobbyPage = defineAsyncComponent(() => import('@/components/LobbyPage.vue'))
+const LoginPage = defineAsyncComponent(() => import('@/components/LoginPage.vue'))
+
+export default {
+  components: {
+    LobbyPage,
+    LoginPage,
+  },
+  async setup() {
+    const userData = ref(await auth.loadUserData())
+    return {
+      userData,
+    }
+  },
+}
+</script>
+
+<style></style>
--- saeroun-web/src/views/HomeView.vue
+++ saeroun-web/src/views/HomeView.vue
@@ -1,20 +1,34 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <h1>This is Saeroun English</h1>
-    <p>hi</p>
+    <h2>This is Saeroun English</h2>
   </div>
-  <HomeForm />
+  <p>{{ result }}</p>
+  <p>{{ data }}</p>
+
+  <Suspense>
+    <template #default>
+      <HomeForm />
+    </template>
+    <template #fallback>
+      <span>Loading...</span>
+    </template>
+  </Suspense>
 </template>
 
 <script>
-// @ is an alias to /src
-import HomeForm from '@/components/HomeForm.vue'
-
+import HomeForm from '@/frames/HomeFrame.vue'
 export default {
   name: 'HomeView',
   components: {
     HomeForm,
+  },
+  data() {
+    return {
+      hasAuth: false,
+      result: '',
+      data: '',
+    }
   },
 }
 </script>
