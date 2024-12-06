a2f02e98c01fc813833a6c517ea11bf7c1a39138
Seung-ryeol Choi <100273844+danielchoi1115@users.noreply.github.com>
2024-12-06 16:38:24 +0900

    Delete saeroun-web directory

--- saeroun-web/src/quasar-user-options.js
+++
@@ -1,9 +0,0 @@
-import './styles/quasar.sass'
-import '@quasar/extras/material-icons/material-icons.css'
-import { QInput, QSelect, QIcon, QField } from 'quasar'
-// To be used on app.use(Quasar, { ... })
-export default {
-  config: {},
-  plugins: {},
-  components: { QInput, QSelect, QIcon, QField },
-}
